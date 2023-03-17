7cc3b97f6b41098179cff7318bbdeacfb329652a
Abylay Sagymbayev <abylaydev@gmail.com>
2023-03-17 21:50:03 +0600

    small fixes

--- src/main/java/kz/spring/laptop_spring/model/Laptop.java
+++ src/main/java/kz/spring/laptop_spring/model/Laptop.java
@@ -3,7 +3,6 @@ package kz.spring.laptop_spring.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Data
 @NoArgsConstructor
