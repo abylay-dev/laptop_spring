70c068f6b1eae65e9392e3fe6217f9521791d8a7
Abylay Sagymbayev <abylaydev@gmail.com>
2023-03-29 22:08:33 +0600

    added annotations to create table in DB

--- src/main/java/kz/spring/laptop_spring/model/Laptop.java
+++ src/main/java/kz/spring/laptop_spring/model/Laptop.java
@@ -1,5 +1,6 @@
 package kz.spring.laptop_spring.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,9 +8,16 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Laptop {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "laptop_id")
     private Integer id;
+
+    @Column(length = 100)
     private String model;
+
     private int price;
     private int count;
 //    private Country country;
